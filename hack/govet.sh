cb74a78a29317a746a0aa77f04df1dba8fde6a95
digambar15 <digambarpat@gmail.com>
2020-05-06 21:19:58 +0530

    modify govet.sh and unit.sh to support hcc

--- hack/govet.sh
+++ hack/govet.sh
@@ -14,7 +14,7 @@ else
     --env IS_CONTAINER=TRUE \
     --volume "${PWD}:/go/src/github.com/metal3-io/baremetal-operator:ro,z" \
     --entrypoint sh \
-    --workdir /go/src/github.com/metal3-io/baremetal-operator \
+    --workdir /go/src/github.com/metal3-io/hardware-classification-controller \
     registry.hub.docker.com/library/golang:1.12 \
-    /go/src/github.com/metal3-io/baremetal-operator/hack/govet.sh "${@}"
+    /go/src/github.com/metal3-io/hardware-classification-controller/hack/govet.sh "${@}"
 fi;
--- hack/unit.sh
+++ hack/unit.sh
@@ -8,19 +8,15 @@ CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-podman}"
 
 if [ "${IS_CONTAINER}" != "false" ]; then
   eval "$(go env)"
-  cd "${GOPATH}"/src/github.com/metal3-io/baremetal-operator
+  cd "${GOPATH}"/src/github.com/metal3-io/hardware-classification-controller
   export XDG_CACHE_HOME="/tmp/.cache"
   go test -v ./pkg/... ./cmd/... -coverprofile "${ARTIFACTS}"/cover.out
 else
   "${CONTAINER_RUNTIME}" run --rm \
     --env IS_CONTAINER=TRUE \
-    --env DEPLOY_KERNEL_URL=http://172.22.0.1/images/ironic-python-agent.kernel \
-    --env DEPLOY_RAMDISK_URL=http://172.22.0.1/images/ironic-python-agent.initramfs \
-    --env IRONIC_ENDPOINT=http://localhost:6385/v1/ \
-    --env IRONIC_INSPECTOR_ENDPOINT=http://localhost:5050/v1/ \
-    --volume "${PWD}:/go/src/github.com/metal3-io/baremetal-operator:ro,z" \
+    --volume "${PWD}:/go/src/github.com/metal3-io/hardware-classification-controller:ro,z" \
     --entrypoint sh \
-    --workdir /go/src/github.com/metal3-io/baremetal-operator \
+    --workdir /go/src/github.com/metal3-io/hardware-classification-controller \
     registry.hub.docker.com/library/golang:1.12 \
-    /go/src/github.com/metal3-io/baremetal-operator/hack/unit.sh "${@}"
+    /go/src/github.com/metal3-io/hardware-classification-controller/hack/unit.sh "${@}"
 fi;
